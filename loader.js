06687fd9ee1ddab990b489f05a5ee7e9c3a33798
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-07-01 21:30:00 +0530

    docs(comment): fix typo

--- loader.js
+++ loader.js
@@ -1,13 +1,10 @@
-// <script type="module" src="./front-dev-tools/loader.js"></script> -- add this in you html file
-
-// loader.js 
 (async function () {
   await Promise.all([
-    // debug-borders varient 2
+    // debug-borders variant 2
     import('./debug-borders/debug-borders-toggle-alt+b-va2.js'),
-    // hover-classes varient 5
+    // hover-classes variant 5
     import('./hover-classes/hover-classes-cursor-toggle-alt+h-va5.js'),
-    // label-semantic-tags varient 2
+    // label-semantic-tags variant 2
     import('./label-semantic-tags/label-semantic-tags-toggle-alt+s-va2.js')
   ]);
 })();
